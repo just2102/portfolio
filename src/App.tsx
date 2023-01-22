c68f680295070be05bb5addb0d0265125c2172cc
just2102 <thesixteensaltines@gmail.com>
2023-01-22 18:52:09 +0900

    fix home padding; switch to hashrouter for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route, } from "react-router";
-import { BrowserRouter, Navigate } from "react-router-dom";
+import { HashRouter, Navigate } from "react-router-dom";
 import "./App.css";
 import Contact from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
@@ -26,9 +26,9 @@ function App() {
 const AppContainer = () => {
   return ( 
     <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
    );
 }
